0f14bcbe87e6f3b7c8666e2e80154b253eb7849e
Ke Wang <ke.wang@sun.com>
2009-05-27 17:50:59 +0800

    Change license from LGPLv3 to LGPLv2.1

--- jni/src/jawtoplevel.h
+++ jni/src/jawtoplevel.h
@@ -1,21 +1,20 @@
 /*
  * Java ATK Wrapper for GNOME
- * Copyright 2009 Sun Microsystems Inc.
+ * Copyright (C) 2009 Sun Microsystems Inc.
  *
- * This file is part of Java ATK Wrapper.
-
- * Java ATK Wrapper is free software: you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * Java ATK Wrapper is distributed in the hope that it will be useful,
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General Public License
- * along with Java ATK Wrapper.  If not, see <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifndef _JAW_TOPLEVEL_H_
