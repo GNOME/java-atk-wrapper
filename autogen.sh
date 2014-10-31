63ec9208d5d004244cc270bf1510ff5273976a03
Alejandro Piñeiro <apinheiro@igalia.com>
2014-10-31 14:22:48 +0100

    Update autogen.sh
    Mostly a C&P from gtk, removing the gtkdoc check, as jaw doesnt
    use it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=739427
    
    Signed-off-by: Magdalen Berns <m.berns@thismagpie.com>

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,38 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 
-REQUIRED_AUTOMAKE_VERSION=1.6
+olddir=`pwd`
+cd "$srcdir"
 
-PKG_NAME=java-atk-wrapper
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+mkdir -p m4
+
+PKGCONFIG=`which pkg-config`
+if test -z "$PKGCONFIG"; then
+        echo "*** pkg-config not found, please install it ***"
+        exit 1
+fi
+
+pkg-config --print-errors gobject-introspection-1.0
+if [ $? != 0 ]; then
+	echo "You probably need to install 'libgirepository1.0-dev'"
+	exit 1
+fi
+
+# README and INSTALL are required by automake, but may be deleted by clean
+# up rules. to get automake to work, simply touch these here, they will be
+# regenerated from their corresponding *.in files by ./configure anyway.
+touch README INSTALL
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please install it ***"
+        exit 1
+else
+        autoreconf --force --install --verbose || exit $?
+fi
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
