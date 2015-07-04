f35407ada9840101e1634c850bea6171cd40a4f2
Magdalen Berns <m.berns@thismagpie.com>
2015-07-04 22:17:50 +0100

    Build: Address warning in autogen.sh
    Bug: https://bugzilla.gnome.org/show_bug.cgi?id=751958

--- autogen.sh
+++ autogen.sh
@@ -10,7 +10,7 @@ cd "$srcdir"
 mkdir -p m4
 
 AUTORECONF=`which autoreconf`
-if test -z $AUTORECONF; then
+if test -z ${AUTORECONF}; then
         echo "*** No autoreconf found, please intall it ***"
         exit 1
 else
