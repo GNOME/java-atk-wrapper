0dd40b6945429a1f7bdd688a1f2b1e480b564c53
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-05-01 22:20:53 +0000

    Fix missing reference addition on children_changed::add
    Fixes https://bugzilla.gnome.org/show_bug.cgi?id=793622

--- jni/src/AtkWrapper.c
+++ jni/src/AtkWrapper.c
@@ -644,6 +644,8 @@ signal_emit_handler (gpointer p)
                             "children_changed::add",
                             child_index,
                             para->child_impl);
+        if (G_OBJECT(atk_obj) != NULL)
+          g_object_ref(G_OBJECT(atk_obj));
         break;
       }
       case Sig_Object_Children_Changed_Remove:
